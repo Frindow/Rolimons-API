93df583073fe04dddd42bd63e4fe6a7e40f7fe14
Frindow <69374353+Frindow@users.noreply.github.com>
2022-10-10 23:12:03 -0400

    Update Example Script.lua

--- Example Script.lua
+++ Example Script.lua
@@ -1,35 +1,68 @@
-local api = require(game:GetService("ReplicatedStorage").API)
+local items = require(game:GetService("ReplicatedStorage")["Rolimon's API"].Items)
+local userDetails = require(game:GetService("ReplicatedStorage")["Rolimon's API"].UserDetails)
+local userAssets = require(game:GetService("ReplicatedStorage")["Rolimon's API"].UserAssets)
 
-api.fetchItems() -- Generates a table based off of the Rolimon's item API 
+items.fetchItems() -- Generates a table based off of the Rolimon's item API 
 print("--------------------")
 print("Generated table:")
-print(api.fetchedItems) -- Prints the generated table
+print(items.Items) -- Prints the generated table
 
-api.fetchItemDetails(2409285794) -- Fetches item details for "Playful Vampire"
+items.fetchItemDetails(2409285794) -- Fetches item details for "Playful Vampire"
 print("--------------------")
-print("Fetched name: " .. api.fetchedName) -- Prints the fetched items name
-print("Fetched acronym: " .. api.fetchedAcronym) -- Prints the fetched items acronym
-print("Fetched RAP: " .. api.fetchedRAP) -- Prints the fetched items RAP
-print("Fetched value: " .. api.fetchedValue) -- Prints the fetched items value
+print("Name: " .. items.Name) -- Prints the  items name
+print("Acronym: " .. items.Acronym) -- Prints the  items acronym
+print("RAP: " .. items.RAP) -- Prints the  items RAP
+print("Value: " .. items.Value) -- Prints the  items value
+print("Default value: " .. items.DefaultValue) -- Prints the  items value
+print("Demand: " .. items.Demand) -- Prints the  items demand
+print("Trend: " .. items.Trend) -- Prints the  items trend
+print("Projected:") -- Prints a boolean determining if the item is projected
+print(items.Projected)
+print("Hyped:") -- Prints a boolean determining if the item is hyped
+print(items.Hyped)
+print("Rare:") -- Prints a boolean determining if the item is rare
+print(items.Rare)
 
-api.getRAPChangeFromItem(1000, 2409285794) -- Generates the expected price and other details if "Playful Vampire" were to be sold at 1000 robux
+items.getRAPChangeFromItem(1000, 2409285794) -- Generates the expected price and other details if "Playful Vampire" were to be sold at 1000 robux
 print("--------------------")
-print("Original RAP: " .. api.originalRAP)
-print("Expected RAP: " .. api.expectedRAP) -- Prints the expected RAP
-print("Percent change: " .. api.percentChange .. "%") -- Prints the percent the RAP increased or decreased
+print("Original RAP: " .. items.originalRAP)
+print("Expected RAP: " .. items.expectedRAP) -- Prints the expected RAP
+print("Percent change: " .. items.percentChange .. "%") -- Prints the percent the RAP increased or decreased
 print("Is gaining RAP:")
-print(api.isGainingRAP) -- Prints a boolean on whether or not the RAP is increasing or decreasing (true is gaining, false is decreasing)
-print("RAP change: " .. api.RAPChange) -- The change in RAP
+print(items.isGainingRAP) -- Prints a boolean on whether or not the RAP is increasing or decreasing (true is gaining, false is decreasing)
+print("RAP change: " .. items.RAPChange) -- The change in RAP
 
-api.getRAPChange(1000, 25000) -- Generates the expected price and other details if a limited were to be sold at 1000 with the current rap being 25,000
+items.getRAPChange(1000, 25000) -- Generates the expected price and other details if a limited were to be sold at 1000 with the current rap being 25,000
 print("--------------------")
-print("Original RAP: " .. api.originalRAP)
-print("Expected RAP: " .. api.expectedRAP) -- Prints the expected RAP
-print("Percent change: " .. api.percentChange .. "%") -- Prints the percent the RAP increased or decreased
+print("Original RAP: " .. items.originalRAP)
+print("Expected RAP: " .. items.expectedRAP) -- Prints the expected RAP
+print("Percent change: " .. items.percentChange .. "%") -- Prints the percent the RAP increased or decreased
 print("Is gaining RAP:")
-print(api.isGainingRAP) -- Prints a boolean on whether or not the RAP is increasing or decreasing (true is gaining, false is decreasing)
-print("RAP change: " .. api.RAPChange) -- The change in RAP
+print(items.isGainingRAP) -- Prints a boolean on whether or not the RAP is increasing or decreasing (true is gaining, false is decreasing)
+print("RAP change: " .. items.RAPChange) -- The change in RAP
 
-api.updateItemCount() -- Updates the item count variable
+items.updateItemCount() -- Updates the item count variable
 print("--------------------")
-print("Item count: " .. api.itemCount) -- Prints the item count
+print("Item count: " .. items.itemCount) -- Prints the item count
+
+
+userDetails.fetchUserDetails(491970127)
+print("--------------------")
+print("Username: " .. userDetails.Username)
+print("Value: " .. userDetails.Value)
+print("RAP: " .. userDetails.RAP)
+print("Rank: " .. userDetails.Rank)
+print("Has premium:")
+print(userDetails.Premium)
+print("Privacy enabled:")
+print(userDetails.PrivacyEnabled)
+print("Terminated:")
+print(userDetails.Terminated)
+print("Stats updated: " .. userDetails.StatsUpdated)
+print("Last online: " .. userDetails.LastOnline)
+print("Last location" .. userDetails.LastLocation)
+print("Rolibadges:")
+print(userDetails.RoliBadges)
+
+userAssets.fetchPlayerAssets(491970127)
+print(userAssets.Assets)

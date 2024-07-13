6dea681737f19c1dff8004cb290a568ca67c4f38
Frindow <69374353+Frindow@users.noreply.github.com>
2024-07-13 00:10:47 -0400

    Create README.lua

---
+++ Rolimon's API 1.4.0/README.lua
@@ -0,0 +1,34 @@
+                                                                                                                                                                                                                                                                                           --[[
+ ____       _ _                       _          _    ____ ___   __  __           _       _      
+|  _ \ ___ | (_)_ __ ___   ___  _ __ ( )___     / \  |  _ \_ _| |  \/  | ___   __| |_   _| | ___ 
+| |_) / _ \| | | '_ ` _ \ / _ \| '_ \|// __|   / _ \ | |_) | |  | |\/| |/ _ \ / _` | | | | |/ _ \
+|  _ < (_) | | | | | | | | (_) | | | | \__ \  / ___ \|  __/| |  | |  | | (_) | (_| | |_| | |  __/
+|_| \_\___/|_|_|_| |_| |_|\___/|_| |_| |___/ /_/   \_\_|  |___| |_|  |_|\___/ \__,_|\__,_|_|\___|
+ ____            _   _       _   _____     _           _               
+| __ ) _   _ _  | \ | | ___ | |_|  ___| __(_)_ __   __| | _____      __
+|  _ \| | | (_) |  \| |/ _ \| __| |_ | '__| | '_ \ / _` |/ _ \ \ /\ / /
+| |_) | |_| |_  | |\  | (_) | |_|  _|| |  | | | | | (_| | (_) \ V  V / 
+|____/ \__, (_) |_| \_|\___/ \__|_|  |_|  |_|_| |_|\__,_|\___/ \_/\_/  
+       |___/                                                           
+
+
+
+CHANGELOG (7/13/24 - v1.4.0):
+- Migrated to the new Rolimon's API endpoints
+- Fixed an issue where the GetRAPChange() function would return nil if the RAP was increasing
+- Now only uses a single module rather than two
+- Support for tracking recent trade ads
+- Rewrote the entire module
+- Removed GetRAPChangeFromItem(), use GetRAPChange() instead
+- Removed GetPlayerAssets() due to the endpoint being removed
+
+
+
+If you have any questions please leave a reply on this DevForum post:
+https://devforum.roblox.com/t/rolimons-api-module/2015062
+
+Github Repository:
+https://github.com/Frindow/Rolimons-API
+
+Marketplace Asset:
+https://create.roblox.com/store/asset/14762212652/Rolimons-API-Module                                                                                                                                                                                                                                                                                           ]]
